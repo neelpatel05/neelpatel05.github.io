c89f8ad252a96c601f9192dd79628d8687ece13d
Neel Patel <neel.patel6573@gmail.com>
2019-03-04 13:41:18 +0530

    adds exception to ajax

--- js/vuescript.js
+++ js/vuescript.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#app',
     created() {
-        this.fetchData();	
+        this.fetchData();
     },
     data: {
       posts:[],
@@ -9,37 +9,44 @@ var app = new Vue({
     },
     methods:{
         fetchData() {
-            axios.get('https://api.github.com/users/neelpatel05/events').then(response => {
-                var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
-                this.time=loadTime
-                for(var i=0;i<5;i++){
-                    var name=response.data[i].repo.name.split("/")
-                    var createdate=response.data[i].created_at.split("T")
-                    var createdate1=createdate[0].split("-")
-                    var createdate2=createdate1.reverse().join("-")
-                    if (response.data[i].type == "PushEvent"){
-                        
-                        var data={
-                            "created_at":createdate2,
-                            "name":name[1],
-                            "url":response.data[i].repo.url,
-                            "description":response.data[i].payload.description,
-                            "type":response.data[i].type,
-                            "numberofcommits":response.data[i].payload.commits.length,
-                            "commits":response.data[i].payload.commits
+            axios.get('https://api.github.com/users/neelpatel05/events').then(
+                (response) => {
+                    var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
+                    this.time=loadTime
+                    for(var i=0;i<5;i++){
+                        var name=response.data[i].repo.name.split("/")
+                        var createdate=response.data[i].created_at.split("T")
+                        var createdate1=createdate[0].split("-")
+                        var createdate2=createdate1.reverse().join("-")
+                        if (response.data[i].type == "PushEvent"){
+                            
+                            var data={
+                                "created_at":createdate2,
+                                "name":name[1],
+                                "url":response.data[i].repo.url,
+                                "description":response.data[i].payload.description,
+                                "type":response.data[i].type,
+                                "numberofcommits":response.data[i].payload.commits.length,
+                                "commits":response.data[i].payload.commits
+                            }
+                        }else{
+                            var data={
+                                "created_at":createdate2,
+                                "name":name[1],
+                                "url":response.data[i].repo.url,
+                                "description":response.data[i].payload.description,
+                                "type":response.data[i].type
+                            }
                         }
-                    }else{
-                        var data={
-                            "created_at":createdate2,
-                            "name":name[1],
-                            "url":response.data[i].repo.url,
-                            "description":response.data[i].payload.description,
-                            "type":response.data[i].type
-                        }
+                        this.posts.push(data)
                     }
-                    this.posts.push(data)
+                },
+                (error) => {
+                    document.getElementById("events").style="display:none";
+                    var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart;
+                    this.time=loadTime
                 }
-            });
+            );
         }
     }
 })
