7619aa91d4ca69c2bc1cce09743e7c976b529e48
Neel <neel.patel6573@gmail.com>
2019-09-21 11:42:05 -0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-data={
-    name:'Neel Patel',
-    email:'nnpatel@terpmail.umd.edu',
-    detail1:'I am a Graduate student mastering in Cybersecurity from <a href="https://eng.umd.edu/">A. James Clark School of Engineering</a> - <a href="https://umd.edu/">University of Maryland, College Park</a>. My primary interests are laying security in softwares, networks, infromation, penetration testing, finding vulnerabilities and in Machine Learning.',
-    detail2:'I was working as an "Intern-Product and Project" at <a href="http://www.theopeneyes.com/">OpenEyes Technologies, Inc</a> from Jan 2019 - April 2019. At OpenEyes, I was working with a team-mate to develop a Machine Learning based project named <a href="https://github.com/neelpatel05/anti-smokify">"Anti-Smokify"</a> to identify smoking scenes from videos and movies. '
-}
-
 window.addEventListener('load', async e => {
     if ('serviceWorker' in navigator) {
         try {
@@ -19,38 +12,6 @@ window.onload = function(){
     browser()
     console.log("Hey there developer!")
     scramble1()
-}
-
-function scramble1(){
-    var x = 0;
-    var name = document.getElementById("name").textContent
-    var email = document.getElementById("email").textContent
-    var detail1 = document.getElementById("detail1").textContent
-    var detail2 = document.getElementById("detail2").textContent
-    var intervalID=setInterval(function () {
-        document.getElementById("name").innerHTML = scramble(name)
-        document.getElementById("email").innerHTML = scramble(email)
-        document.getElementById("detail1").innerHTML = scramble(detail1)
-        document.getElementById("detail2").innerHTML = scramble(detail2)
-        if (++x === 20) {
-            window.clearInterval(intervalID);
-            document.getElementById("name").innerHTML = data.name
-            document.getElementById("detail1").innerHTML = data.detail1
-            document.getElementById("detail2").innerHTML = data.detail2
-        }
-    }, 75);
-}
-
-function unscrambleEmail(){
-    var x = 0;
-    var email = document.getElementById("email").textContent
-    var intervalID=setInterval(function () {
-        document.getElementById("email").innerHTML = scramble(email)
-        if (++x === 20) {
-            window.clearInterval(intervalID);
-            document.getElementById("email").innerHTML = data.email
-        }
-    }, 100);
 }
 
 function browser(){
